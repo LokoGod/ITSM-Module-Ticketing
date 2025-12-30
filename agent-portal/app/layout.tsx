cafddeea10a1fd692d2094040b8bb0cc135875f6
Mayukha Palihena <87293241+LokoGod@users.noreply.github.com>
2025-12-30 16:42:36 +0530

    Outfit.font

--- agent-portal/app/layout.tsx
+++ agent-portal/app/layout.tsx
@@ -1,19 +1,9 @@
 import type { Metadata } from "next";
-import { Geist, Geist_Mono, Inter } from "next/font/google";
+import { Geist, Geist_Mono, Inter, Outfit } from "next/font/google";
 import "./globals.css";
 import Sidebar from "@/components/sidebar";
 
-const inter = Inter({ subsets: ['latin'], variable: '--font-sans' });
-
-const geistSans = Geist({
-  variable: "--font-geist-sans",
-  subsets: ["latin"],
-});
-
-const geistMono = Geist_Mono({
-  variable: "--font-geist-mono",
-  subsets: ["latin"],
-});
+const outfit = Outfit({ variable: '--font-sans' });
 
 export const metadata: Metadata = {
   title: "Create Next App",
@@ -26,10 +16,8 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="en" className={inter.variable}>
-      <body
-        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
-      >
+    <html lang="en" className={outfit.variable}>
+      <body>
         <Sidebar>
           {children}
         </Sidebar>
